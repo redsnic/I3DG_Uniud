b36cfb44df0483aca9302a747a941c940b803bf9
Lorenzo Iuri <iuri.lorenzo@spes.uniud.it>
2019-05-04 15:10:32 +0200

    added selectors

--- Lorenzo/pokeball/poke.js
+++ Lorenzo/pokeball/poke.js
@@ -14,31 +14,38 @@ let vs = document.getElementById("vertex").textContent;
 let fs = document.getElementById("fragment").textContent;
 
 let textureTopMaterial = {
-    diffuse : loadTexture("texture/poke/pokeball_diffuse.png"),
-    specular : loadTexture("texture/poke/pokeball_spec.png"),
-    normal : loadTexture("texture/poke/pokeball_normal.png"),
-    roug : loadTexture("texture/poke/pokeball_roug.png"),
+    diffuse: loadTexture("texture/poke/pokeball_diffuse.png"),
+    specular: loadTexture("texture/poke/pokeball_spec.png"),
+    normal: loadTexture("texture/poke/pokeball_normal.png"),
+    roug: loadTexture("texture/poke/pokeball_roug.png"),
 }
 
 let textureBottomMaterial = {
-    diffuse : loadTexture("texture/poke/pokeball_diffuse.png"),
-    specular : loadTexture("texture/poke/pokeball_spec.png"),
-    normal : loadTexture("texture/poke/pokeball_normal.png"),
-    roug : loadTexture("texture/poke/pokeball_roug.png"),
+    diffuse: loadTexture("texture/poke/pokeball_diffuse.png"),
+    specular: loadTexture("texture/poke/pokeball_spec.png"),
+    normal: loadTexture("texture/poke/pokeball_normal.png"),
+    roug: loadTexture("texture/poke/pokeball_roug.png"),
 }
 
 let textureRingMaterial = {
-    diffuse : loadTexture("texture/poke/pokeball_diffuse.png"),
-    specular : loadTexture("texture/poke/pokeball_spec.png"),
-    normal : loadTexture("texture/poke/pokeball_normal.png"),
-    roug : loadTexture("texture/poke/pokeball_roug.png"),
+    diffuse: loadTexture("texture/poke/pokeball_diffuse.png"),
+    specular: loadTexture("texture/poke/pokeball_spec.png"),
+    normal: loadTexture("texture/poke/pokeball_normal.png"),
+    roug: loadTexture("texture/poke/pokeball_roug.png"),
 }
 
 let textureButtonMaterial = {
-    diffuse : loadTexture("texture/poke/pokeball_diffuse.png"),
-    specular : loadTexture("texture/poke/pokeball_spec.png"),
-    normal : loadTexture("texture/poke/pokeball_normal.png"),
-    roug : loadTexture("texture/poke/pokeball_roug.png"),
+    diffuse: loadTexture("texture/poke/pokeball_diffuse.png"),
+    specular: loadTexture("texture/poke/pokeball_spec.png"),
+    normal: loadTexture("texture/poke/pokeball_normal.png"),
+    roug: loadTexture("texture/poke/pokeball_roug.png"),
+}
+
+let textures = {
+    top: "Pokeball",
+    bottom: "Pokeball",
+    ring: "Pokeball",
+    button: "Pokeball",
 }
 
 // default: white, 1.0 intensity
@@ -85,18 +92,10 @@ let uniformsButtonMaterial = {
     normalMap: { type: "t", value: textureButtonMaterial.normal },
 }
 
-/*
-// steel texture
-let textureDiffuseSteel = loadTexture("texture/steel/CorrugatedSteel01_col.jpg");
-let textureSpecularSteel = loadTexture("texture/steel/CorrugatedSteel01_met.jpg");
-let textureNormalSteel = loadTexture("texture/steel/CorrugatedSteel01_nrm.jpg");
-let textureRougSteel = loadTexture("texture/steel/CorrugatedSteel01_rgh.jpg");*/
-
-
-topMaterial = new THREE.ShaderMaterial( {uniforms: uniformsTopMaterial, vertexShader: vs, fragmentShader: fs });
-bottomMaterial = new THREE.ShaderMaterial( {uniforms: uniformsBottomMaterial, vertexShader: vs, fragmentShader: fs });
-ringMaterial = new THREE.ShaderMaterial( {uniforms: uniformsRingMaterial, vertexShader: vs, fragmentShader: fs });
-buttonMaterial = new THREE.ShaderMaterial( {uniforms: uniformsButtonMaterial, vertexShader: vs, fragmentShader: fs });
+topMaterial = new THREE.ShaderMaterial({ uniforms: uniformsTopMaterial, vertexShader: vs, fragmentShader: fs });
+bottomMaterial = new THREE.ShaderMaterial({ uniforms: uniformsBottomMaterial, vertexShader: vs, fragmentShader: fs });
+ringMaterial = new THREE.ShaderMaterial({ uniforms: uniformsRingMaterial, vertexShader: vs, fragmentShader: fs });
+buttonMaterial = new THREE.ShaderMaterial({ uniforms: uniformsButtonMaterial, vertexShader: vs, fragmentShader: fs });
 
 topMaterial.vertexTangents = true; topMaterial.needsUpdate = true;
 bottomMaterial.vertexTangents = true; bottomMaterial.needsUpdate = true;
@@ -135,15 +134,16 @@ let lightMesh = new THREE.Mesh(new THREE.SphereGeometry(1, 16, 16), new THREE.Me
 lightMesh.position.set(7.0, 7.0, 7.0);
 
 uniformsTopMaterial.pointLightPosition.value = new THREE.Vector3(lightMesh.position.x, lightMesh.position.y, lightMesh.position.z);
-uniformsTopMaterial.clight.value = new THREE.Vector3(1.0,1.0,1.0);
+uniformsTopMaterial.clight.value = new THREE.Vector3(1.0, 1.0, 1.0);
 uniformsBottomMaterial.pointLightPosition.value = new THREE.Vector3(lightMesh.position.x, lightMesh.position.y, lightMesh.position.z);
-uniformsBottomMaterial.clight.value = new THREE.Vector3(1.0,1.0,1.0);
+uniformsBottomMaterial.clight.value = new THREE.Vector3(1.0, 1.0, 1.0);
 uniformsRingMaterial.pointLightPosition.value = new THREE.Vector3(lightMesh.position.x, lightMesh.position.y, lightMesh.position.z);
-uniformsRingMaterial.clight.value = new THREE.Vector3(1.0,1.0,1.0);
+uniformsRingMaterial.clight.value = new THREE.Vector3(1.0, 1.0, 1.0);
 uniformsButtonMaterial.pointLightPosition.value = new THREE.Vector3(lightMesh.position.x, lightMesh.position.y, lightMesh.position.z);
-uniformsButtonMaterial.clight.value = new THREE.Vector3(1.0,1.0,1.0);
+uniformsButtonMaterial.clight.value = new THREE.Vector3(1.0, 1.0, 1.0);
 
 let stats = new Stats();
+let gui;
 
 function init() {
 
@@ -161,9 +161,19 @@ function init() {
     controls.enablePan = true;
     controls.update();
 
+    window.addEventListener('resize', onResize, false);
+
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
     document.body.appendChild(stats.domElement);
+}
+
+function onResize() {
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = (window.innerWidth / window.innerHeight);
+    camera.updateProjectionMatrix();
+
 }
 
 function loadTexture(file) {
@@ -185,9 +195,95 @@ function update() {
 }
 
 function render() {
+    updateUniforms();
     renderer.render(scene, camera);
 }
 
+function clearGui() {
+    if (gui) gui.destroy();
+    gui = new dat.GUI();
+    gui.open();
+}
+
+function buildGui() {
+    clearGui();
+    let lightSettings = gui.addFolder('Light Parameters');
+    lightSettings.add(lightParameters, 'red').min(0).max(1).onChange(function (newVal) { render() });
+    lightSettings.add(lightParameters, 'green').min(0).max(1).onChange(function (newVal) { render() });
+    lightSettings.add(lightParameters, 'blue').min(0).max(1).onChange(function (newVal) { render() });
+    lightSettings.add(lightParameters, 'intensity').min(0).max(10).onChange(function (newVal) { render() });
+
+    let textureSettings = gui.addFolder('Texture parameters');
+    textureSettings.add(textures, 'top', ["Pokeball", "Masterball", "Metal"]).onChange(
+        function (newVal) {
+            switch (newVal) {
+                case "Pokeball":
+                    textureTopMaterial.diffuse = loadTexture("texture/poke/pokeball_diffuse.png");
+                    textureTopMaterial.specular = loadTexture("texture/poke/pokeball_spec.png");
+                    textureTopMaterial.normal = loadTexture("texture/poke/pokeball_normal.png");
+                    textureTopMaterial.roug = loadTexture("texture/poke/pokeball_roug.png");
+                    break;
+                case "Masterball":
+                    textureTopMaterial.diffuse = loadTexture("texture/master/master_diffuse.png");
+                    textureTopMaterial.specular = loadTexture("texture/poke/pokeball_spec.png");
+                    textureTopMaterial.normal = loadTexture("texture/master/master_normal.png");
+                    textureTopMaterial.roug = loadTexture("texture/poke/pokeball_roug.png");
+                    break;
+                case "Metal":
+                    textureTopMaterial.diffuse = loadTexture("texture/steel/CorrugatedSteel01_col.jpg");
+                    textureTopMaterial.specular = loadTexture("texture/steel/CorrugatedSteel01_met.jpg");
+                    textureTopMaterial.normal = loadTexture("texture/steel/CorrugatedSteel01_nrm.jpg");
+                    textureTopMaterial.roug = loadTexture("texture/steel/CorrugatedSteel01_rgh.jpg");
+                    break;
+            }
+            topMaterial.needsUpdate = true;
+            render()
+        });
+    textureSettings.add(textures, 'bottom', ["Pokeball", "Masterball", "Metal"]).onChange(
+        function (newVal) {
+            switch (newVal) {
+                case "Pokeball":
+                    textureBottomMaterial.diffuse = loadTexture("texture/poke/pokeball_diffuse.png");
+                    textureBottomMaterial.specular = loadTexture("texture/poke/pokeball_spec.png");
+                    textureBottomMaterial.normal = loadTexture("texture/poke/pokeball_normal.png");
+                    textureBottomMaterial.roug = loadTexture("texture/poke/pokeball_roug.png");
+                    break;
+                case "Masterball":
+                    textureBottomMaterial.diffuse = loadTexture("texture/master/master_diffuse.png");
+                    textureBottomMaterial.specular = loadTexture("texture/poke/pokeball_spec.png");
+                    textureBottomMaterial.normal = loadTexture("texture/master/master_normal.png");
+                    textureBottomMaterial.roug = loadTexture("texture/poke/pokeball_roug.png");
+                    break;
+                case "Metal":
+                    textureBottomMaterial.diffuse = loadTexture("texture/steel/CorrugatedSteel01_col.jpg");
+                    textureBottomMaterial.specular = loadTexture("texture/steel/CorrugatedSteel01_met.jpg");
+                    textureBottomMaterial.normal = loadTexture("texture/steel/CorrugatedSteel01_nrm.jpg");
+                    textureBottomMaterial.roug = loadTexture("texture/steel/CorrugatedSteel01_rgh.jpg");
+                    break;
+            }
+            bottomMaterial.needsUpdate = true;
+            render()
+        });
+}
+
+function updateUniforms() {
+
+    //uniforms.clight.value = new THREE.Vector3(
+    //    lightParameters.red * lightParameters.intensity,
+    //    lightParameters.green * lightParameters.intensity,
+    //    lightParameters.blue * lightParameters.intensity);
+    //uniforms.textureRepeat.value = new THREE.Vector2(textureParameters.repeatS, textureParameters.repeatT);
+    uniformsTopMaterial.diffuseMap.value = textureTopMaterial.diffuse;
+    uniformsTopMaterial.specularMap.value = textureTopMaterial.specular;
+    uniformsTopMaterial.rougMap.value = textureTopMaterial.roug;
+    uniformsTopMaterial.normalMap.value = textureTopMaterial.normal;
+
+    uniformsBottomMaterial.diffuseMap.value = textureBottomMaterial.diffuse;
+    uniformsBottomMaterial.specularMap.value = textureBottomMaterial.specular;
+    uniformsBottomMaterial.rougMap.value = textureBottomMaterial.roug;
+    uniformsBottomMaterial.normalMap.value = textureBottomMaterial.normal;
+}
+
 init();
+buildGui();
 update();
-render();
